ff8e92f1114a8505821ba07b551b870cb5cc1dcd
Maksim Zdorenko <94051350+Mcsim97@users.noreply.github.com>
2025-05-23 01:18:45 +0300

    Update app.js
    add js

--- js/app.js
+++ js/app.js
@@ -3,21 +3,3 @@
     "/photo/photo_2025-05-18_14-20-49.jpg",
     "/photo/photo_2025-05-18_14-35-16.jpg"
   ];
-
-  let currentIndex = 0;
-  const imgElement = document.getElementById("slider-image");
-
-  function showImage(index) {
-    imgElement.src = images[index];
-  }
-
-  function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
-  }
-
-  function prevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    showImage(currentIndex);
-  }
-
